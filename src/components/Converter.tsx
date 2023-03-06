8e704470cce610277aadd32468b641e11f5099ef
Ajarek <ajarek@poczta.onet.pl>
2023-03-06 21:47:19 +0100

    add logic & css

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,36 +1,86 @@
 import React, { useEffect, useState } from 'react'
 import data from '../assets/data.json'
 
-
 const Converter = () => {
- const [currency ,setCurrency]=useState<string[]>([])
- const [course,setCourse]=useState<string[]>([])
- const typeData: any=data.rates
- useEffect(()=>{
-  const keys: string[] = Object.keys(typeData)
-  const values: string[] = Object.values(typeData)
+  const [currency, setCurrency] = useState<[string, string][]>([])
+  const [result, setResult] = useState<string>('3.67')
+  const [result2, setResult2] = useState<string>('3.67')
+  const [inputValue, setInputValue] = useState<string>('1.00')
+  const [inputValue2, setInputValue2] = useState<string>('1.00')
+  const typeData: any = data.rates
 
- setCurrency(keys)
- setCourse(values)
- },[])
+  useEffect(() => {
+    const entries: [string, string][] = Object.entries(typeData)
+    setCurrency(entries)
+  }, [])
 
-
-console.log(currency);
-console.log(course);
-
-
+  const handleResult = (e: { target: { value: any } }) => {
+    setResult(e.target.value) 
+     setInputValue(( Number(inputValue2) * Number(e.target.value) /Number(result2)).toFixed(2))
+    
+  }
+  const handleResult2 = (e: { target: { value: any } }) => {
+    setResult2(e.target.value) 
+   
+    setInputValue2(( Number(inputValue) * Number(e.target.value) /Number(result)).toFixed(2))
+   
+  }
+ 
+ 
+ 
   return (
-
-
     <div className='converter'>
-     <select name="" id="">
-      {currency.map((el)=>{
-        return(
-          <option value={el}>{el}</option>
-        )
-      })}
-     </select>
+      <div className='wrapper-converter'>
+        <input
+          type='number'
+          step={'0.01'}
+          value={inputValue}
+          onChange={(e) => {setInputValue(e.target.value); setInputValue2(( Number(e.target.value) * Number(result2) /Number(result)).toFixed(2))}}
+        />
+        <select
+          name=''
+          id=''
+          onChange={handleResult}
+        >
+          {currency.map((el) => {
+            return (
+              <option
+                key={el[0]}
+                value={el[1]}
+              >
+                {el[0]}
+              </option>
+            )
+          })}
+        </select>
+       
       </div>
+      <div className='wrapper-converter'>
+        <input
+          type='number'
+          value={inputValue2}
+          onChange={(e) =>{ setInputValue2(e.target.value); setInputValue(( Number(e.target.value) * Number(result) /Number(result2)).toFixed(2))}}
+          step={'0.01'}
+        />
+        <select
+          name=''
+          id=''
+          onChange={handleResult2}
+        >
+          {currency.map((el) => {
+            return (
+              <option
+                key={el[0]}
+                value={el[1]}
+              >
+                {el[0]}
+              </option>
+            )
+          })}
+        </select>
+        
+      </div>
+    </div>
   )
 }
 
